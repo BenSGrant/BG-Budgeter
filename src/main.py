1979bedf83f35ed7cbde948ab4f2a4a5344ef0d5
BenSGrant <benrupertgrant@gmail.com>
2024-04-27 15:33:39 +0100

    corrected ui syntax

--- src/main.py
+++ src/main.py
@@ -1,12 +1,12 @@
 
-from PyQt6.QtWidgets import QDialog
-from PyQt6.QtCore import Qt, QMutex
+from PyQt5.QtWidgets import QDialog
+from PyQt5.QtCore import Qt, QMutex
 
 from ui import *
 
 class BGBudgeter(QDialog):
     def __init__(self):
         super(QDialog, self).__init__()
-        self.ui = Ui_Dialog()
+        self.ui = Ui_BGBudgeterClass()
         self.ui.setupUi(self)
     
