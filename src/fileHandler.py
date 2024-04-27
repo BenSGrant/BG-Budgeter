058a1aac34b036a852332fdafe0b1a5b197c84d7
BenSGrant <benrupertgrant@gmail.com>
2024-04-27 23:03:08 +0100

    retrieveCategoriesData() method created

--- src/fileHandler.py
+++ src/fileHandler.py
@@ -13,7 +13,7 @@ class FileHandler:
         self.ensureFilesExist()
     
     def ensureFilesExist(self):
-        '''this function checks if save data files do not exist, then creates them if that is the case'''
+        '''This function checks if save data files do not exist, then creates them if that is the case.'''
         if not os.path.exists(self.categoriesPath):
             #dirname returns the directory part, makedirs creates it
             os.makedirs(os.path.dirname(self.categoriesPath), exist_ok=True)
@@ -28,17 +28,16 @@ class FileHandler:
 
 
     def overwrite(self, isCategoriesFile: bool, data):
-        '''completely replace all text in the document.
+        '''Completely replace all text in the document.
 
-            if isCategoriesFile is false, then income file will be assumed
+            If isCategoriesFile is false, then income file will be assumed
 
             File saving convention:
             
-            categories
-            
+            categories:
             name,amount \\n
 
-            income
+            income:
             amount,period,occurences \\n
 
         '''
@@ -52,9 +51,9 @@ class FileHandler:
             fileObj.close()
 
     def appendData(self,isCategoriesFile: bool,data):
-        '''add text to the document
+        '''Add text to the document.
         
-            if isCategoriesFile is false, then income file will be assumed'''
+            If isCategoriesFile is false, then income file will be assumed'''
         if isCategoriesFile:
             fileObj = open(self.categoriesPath, 'a')
             fileObj.write(data)
@@ -66,12 +65,33 @@ class FileHandler:
         
 
     def getData(self, isCategoriesFile: bool):
-        '''retrieve data from document, filter through this raw data as you please.
+        '''Retrieve data from document, filter through this raw data as you please.
         
-            if isCategoriesFile is false, then income file will be assumed'''
+            If isCategoriesFile is false, then income file will be assumed'''
         if isCategoriesFile:
             fileObj = open(self.categoriesPath, 'r')
             return fileObj.readlines()
         else:
             fileObj = open(self.incomePath, 'r')
             return fileObj.readlines()
+    
+    def retrieveCategoriesData(self):
+        '''Takes all data from the file and puts it back into dictionary form. Returns None if there is no data present'''
+        data = self.getData(True)
+        dictToReturn = {}
+        if data is not None:
+            pairs = data[0].split("\n")
+            for pair in pairs:
+                splitPair = pair.split(",")
+                key = splitPair[0]
+                val = splitPair[1]
+                dictToReturn[key] = val
+            return dictToReturn
+        else:
+            return None
+
+    def saveCategoriesData(self, dictionary):
+        # retrieve data first before doing this
+        
+        for key in dictionary:
+            self.appendData(str(key) + "," + str(dictionary[key]) + "\n")
